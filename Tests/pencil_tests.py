1b8a67e502157853f015b07184de97d416a6bad0
Zac Chu <zac.chu@gmail.com>
2019-02-08 18:02:29 -0500

    Fixed a small typo in test_write_on_text_files and updated pencil functionality to work with tests

--- Tests/pencil_tests.py
+++ Tests/pencil_tests.py
@@ -19,9 +19,9 @@ def test_write_on_strings(pencil, expected_writing_on_page, page_before_being_wr
 							[("Hello World!", 							"",								"Hello World!"),
 							 ("She sells sea shells by the sea shore", 	"She sells sea shells",			" by the sea shore")
 							])
-def test_write_on_files(pencil, tmpdir, expected_writing_on_page, page_before_being_writen_on, string_to_write):
+def test_write_on_text_files(pencil, tmpdir, expected_writing_on_page, page_before_being_writen_on, string_to_write):
 	pencil.how_to_write = pencil.write_to_text_file
 	paper = tmpdir.mkdir("sub").join("paper.txt")
-	p.write(page_before_being_writen_on)
+	paper.write(page_before_being_writen_on)
 	pencil.write(string_to_write, paper)
-	assert p.read() == expected_writing_on_page
+	assert paper.read() == expected_writing_on_page
--- pencil.py
+++ pencil.py
@@ -1,4 +1,15 @@
 class Pencil():
 
-	def write(self, to_write, paper):
+	def __init__(self):
+		self.how_to_write = None
+
+	def write_to_string(self, to_write, paper):
 		paper[0] += to_write
+
+	def write_to_text_file(self, to_write, paper):
+		with open(paper, "a") as file:
+			file.write(to_write)
+
+
+	def write(self, to_write, paper):
+		self.how_to_write(to_write, paper)
