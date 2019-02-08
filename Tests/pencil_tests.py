65438b699efcef653843e1fd7f0c02798855612e
Zac Chu <zac.chu@gmail.com>
2019-02-08 14:34:57 -0500

    Added test stubs for writing functionality

---
+++ Tests/pencil_tests.py
@@ -0,0 +1,21 @@
+import unittest
+
+class PencilTests(unittest.TestCase):
+
+	# Tests for Write Section of problem statment
+	def setup(self):
+		self.pencil = pencil()
+
+	def tearDown(self):
+		pass
+
+	def test_write_on_string(self):
+		pass
+
+	def test_write_with_existing_text(self):
+		pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
