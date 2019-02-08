3b50f0b342ba4befb3ea145177a24713eeda4c18
Zac Chu <zac.chu@gmail.com>
2019-02-08 16:26:07 -0500

    Wrote basic writing tests.

--- Tests/pencil_tests.py
+++ Tests/pencil_tests.py
@@ -1,19 +1,26 @@
 import unittest
+from pencil import Pencil
 
 class PencilTests(unittest.TestCase):
 
 	# Tests for Write Section of problem statment
-	def setup(self):
-		self.pencil = pencil()
+	def setUp(self):
+		self.pencil = Pencil()
 
 	def tearDown(self):
 		pass
 
-	def test_write_on_string(self):
-		pass
+	def when_write_on_string(self):
+		test_string = "Hello World!"
+		paper = [""]
+		self.pencil.write(test_string, paper)
+		self.assertEqual(paper[0], test_string)
 
-	def test_write_with_existing_text(self):
-		pass
+	def when_write_with_existing_text(self):
+		test_string = "she sells sea shells by the sea shore"
+		paper = [test_string[:19]]
+		self.pencil.write(test_string[19:], paper)
+		self.assertEqual(paper[0], test_string)
 
 
 
