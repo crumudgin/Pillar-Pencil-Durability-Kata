3872f4c7d090461ce3e5e11570ef5675ddfabdaa
Zac Chu <zac.chu@gmail.com>
2019-02-09 11:40:08 -0500

    refactored pencil to work with the new point degredation tests and all other tests currently in place

--- pencil.py
+++ pencil.py
@@ -1,20 +1,34 @@
 class Pencil():
 
-	def __init__(self):
-		self.how_to_write = None
+	def __init__(self, point_durability):
+		self.how_to_write = self.write_to_string
+		self.point_durability = point_durability
+
+	def calc_char_to_write(self, potential_char_to_write, char_in_paper):
+		if potential_char_to_write != char_in_paper:
+			self.point_durability -= 1
+			if potential_char_to_write.isupper():
+				self.point_durability -= 1
+			if self.point_durability < 0:
+				self.point_durability = 0
+				return char_in_paper
+		return potential_char_to_write
 
 	def write_from_starting_point(self, to_write, paper_text, string_starting_point):
-		new_paper_text = paper_text[:min(len(paper_text), string_starting_point)]
+		if string_starting_point is None:
+			string_starting_point = len(paper_text)
 		if len(paper_text) < string_starting_point:
-			new_paper_text += " " * (string_starting_point - len(paper_text))
+			paper_text += " " * (string_starting_point - len(paper_text))
+		new_paper_text = paper_text[:string_starting_point]
 		for index, char in enumerate(to_write):
 			paper_index = string_starting_point + index
 			if paper_index < len(paper_text) and char != " " and paper_text[paper_index] != " ":
-				new_paper_text += "@"
+				new_paper_text += self.calc_char_to_write("@", paper_text[paper_index])
 			elif paper_index < len(paper_text):
-				new_paper_text += max(char, paper_text[paper_index])
+				new_paper_text += self.calc_char_to_write(max(char, paper_text[paper_index]), paper_text[paper_index])
 			else:
-				new_paper_text += char
+				new_paper_text += self.calc_char_to_write(char, " ")
+			
 		new_paper_text += paper_text[paper_index+1:]
 		return new_paper_text
 
