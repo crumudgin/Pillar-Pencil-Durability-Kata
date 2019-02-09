75701b59d4f12032347df52e790505126596d66b
Zac Chu <zac.chu@gmail.com>
2019-02-09 16:01:50 -0500

    removed all code related to writing to non string objects

--- pencil.py
+++ pencil.py
@@ -1,7 +1,6 @@
 class Pencil():
 
 	def __init__(self, point_durability, length):
-		self.how_to_write = self.write_to_string
 		self.point_durability = point_durability
 		self.max_point_durrability = point_durability
 		self.length = length
@@ -16,39 +15,24 @@ class Pencil():
 				return char_in_paper
 		return potential_char_to_write
 
-	def write_from_starting_point(self, to_write, paper_text, string_starting_point):
+	def write(self, to_write, paper, string_starting_point):
 		if string_starting_point is None:
-			string_starting_point = len(paper_text)
-		if len(paper_text) < string_starting_point:
-			paper_text += " " * (string_starting_point - len(paper_text))
-		new_paper_text = list(paper_text[:string_starting_point])
+			string_starting_point = len(paper)
+		if len(paper) < string_starting_point:
+			paper += " " * (string_starting_point - len(paper))
+		new_paper_text = list(paper[:string_starting_point])
 
 		paper_index = 0
 		for index, char in enumerate(to_write):
 			paper_index = string_starting_point + index
-			if paper_index < len(paper_text) and char != " " and paper_text[paper_index] != " ":
-				new_paper_text.append(self.__calc_char_to_write("@", paper_text[paper_index]))
-			elif paper_index < len(paper_text):
-				new_paper_text.append(self.__calc_char_to_write(max(char, paper_text[paper_index]), paper_text[paper_index]))
+			if paper_index < len(paper) and char != " " and paper[paper_index] != " ":
+				new_paper_text.append(self.__calc_char_to_write("@", paper[paper_index]))
+			elif paper_index < len(paper):
+				new_paper_text.append(self.__calc_char_to_write(max(char, paper[paper_index]), paper[paper_index]))
 			else:
 				new_paper_text.append(self.__calc_char_to_write(char, " "))
 
-		return "".join(new_paper_text) + paper_text[paper_index+1:]
-
-	def write_to_string(self, to_write, paper, string_starting_point, params):
-		paper[0] = self.write_from_starting_point(to_write, paper[0], string_starting_point)
-
-	def write_to_text_file(self, to_write, paper, string_starting_point, params):
-		with open(paper, "r+") as file:
-			paper_text = self.write_from_starting_point(to_write, paper.read(), string_starting_point)
-			file.write(paper_text)
-
-	def write_to_object_variable(self, to_write, paper, string_starting_point, paper_var):
-		place_to_write = getattr(paper, paper_var)
-		setattr(paper, paper_var, self.write_from_starting_point(to_write, place_to_write, string_starting_point))
-
-	def write(self, to_write, paper, string_starting_point, params=None):
-		self.how_to_write(to_write, paper, string_starting_point, params)
+		return "".join(new_paper_text) + paper[paper_index+1:]
 
 	def sharpen(self):
 		if self.length <= 0:
@@ -57,7 +41,8 @@ class Pencil():
 		self.point_durability = self.max_point_durrability
 
 	def erase(self, paper, to_erase):
-		paper_text = paper[0]
-		location_of_to_erase = paper_text.rfind(to_erase)
-		if location_of_to_erase != -1:
-			paper[0] = paper_text[:location_of_to_erase] + ("" * len(to_erase)) + paper_text[location_of_to_erase + len(to_erase):]
+		location_of_to_erase = paper.rfind(to_erase)
+		if location_of_to_erase == -1:
+			return paper
+		return paper[:location_of_to_erase] + ("" * len(to_erase)) + paper[location_of_to_erase + len(to_erase):]
+			
