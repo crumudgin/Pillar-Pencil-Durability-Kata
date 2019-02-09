340b6584d2fb1667c696d5115c343bd552bf700d
Zac Chu <zac.chu@gmail.com>
2019-02-09 14:11:59 -0500

    wrote tests for pencil sharpening

--- Tests/pencil_tests.py
+++ Tests/pencil_tests.py
@@ -25,42 +25,52 @@ point_degradation_test_data = 	[("test",			0,	4,	"",				"test",				None),
 								("",				10,	10,	"",				"",					None)
 								]
 
+sharpen_test_labels = "expected_writing_on_page, expected_point_value, starting_point_value, expected_length, starting_length, page_before_being_writen_on, string_to_write, string_starting_point"
+
+sharpen_test_data = 	[("",		1,	1,	0,	1,	"",	"",		None),
+						("Test",	5,	5,	0,	1,	"",	"Test",	None),
+						("Test",	5,	5,	1,	2,	"",	"Test",	None),
+						("Test",	0,	5,	0,	0,	"",	"Test",	None),
+						("Test",	1,	6,	0,	0,	"",	"Test",	None)
+						]
+
+
 """
 A pencil fixture to be shared by the rest of the test cases
 """
 @pytest.fixture
-def durable_pencil():
-	return Pencil(10000)
+def pencil():
+	return Pencil(10000, 100)
 
 """
 A series of tests to test that the pencil can write to a string
 """
 @pytest.mark.parametrize(writing_test_labels, writing_test_data)
-def test_write_on_strings(durable_pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
+def test_write_on_strings(pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
 	paper = [page_before_being_writen_on]
-	durable_pencil.write(string_to_write, paper, string_starting_point)
+	pencil.write(string_to_write, paper, string_starting_point)
 	assert expected_writing_on_page == paper[0]
 
 """
 A series of tests to test that the pencil can write to a file
 """
 @pytest.mark.parametrize(writing_test_labels, writing_test_data)
-def test_write_on_text_files(durable_pencil, tmpdir, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
-	durable_pencil.how_to_write = durable_pencil.write_to_text_file
+def test_write_on_text_files(pencil, tmpdir, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
+	pencil.how_to_write = pencil.write_to_text_file
 	paper = tmpdir.mkdir("sub").join("paper.txt")
 	paper.write(page_before_being_writen_on)
-	durable_pencil.write(string_to_write, paper, string_starting_point)
+	pencil.write(string_to_write, paper, string_starting_point)
 	assert paper.read() == expected_writing_on_page
 
 @pytest.mark.parametrize(writing_test_labels, writing_test_data)
-def test_write_on_paper_object(durable_pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
+def test_write_on_paper_object(pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
 	class Paper():
 		def __init__(self, text):
 			self.text = text
 
 	paper = Paper(page_before_being_writen_on)
-	durable_pencil.how_to_write = durable_pencil.write_to_object_variable
-	durable_pencil.write(string_to_write, paper, string_starting_point, "text")
+	pencil.how_to_write = pencil.write_to_object_variable
+	pencil.write(string_to_write, paper, string_starting_point, "text")
 	assert paper.text == expected_writing_on_page
 
 """
@@ -68,8 +78,17 @@ A series of tests to test the pencil durrability and what happens when that durr
 """
 @pytest.mark.parametrize(point_degradation_test_labels, point_degradation_test_data)
 def test_point_degradation(expected_writing_on_page, expected_point_value, starting_point_value, page_before_being_writen_on, string_to_write, string_starting_point):
-	pencil = Pencil(starting_point_value)
+	pencil = Pencil(starting_point_value, 1)
 	paper = [page_before_being_writen_on]
 	pencil.write(string_to_write, paper, string_starting_point)
 	assert expected_writing_on_page == paper[0]
 	assert expected_point_value == pencil.point_durability
+
+@pytest.mark.parametrize(sharpen_test_labels, sharpen_test_data)
+def test_sharpen(expected_writing_on_page, expected_point_value, starting_point_value, expected_length, starting_length, page_before_being_writen_on, string_to_write, string_starting_point):
+	pencil = Pencil(starting_point_value, starting_length)
+	paper = [page_before_being_writen_on]
+	pencil.write(string_to_write, paper, string_starting_point)
+	pencil.sharpen()
+	assert pencil.point_durability == expected_point_value
+	assert pencil.length == expected_length
