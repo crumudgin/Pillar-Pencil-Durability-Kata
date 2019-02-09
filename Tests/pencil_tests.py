5e1025e689cde196950e7f5efd026d74b5bab9cb
Zac Chu <zac.chu@gmail.com>
2019-02-09 15:56:40 -0500

    removed tests related to other formats, the pencil will only work on strings now

--- Tests/pencil_tests.py
+++ Tests/pencil_tests.py
@@ -55,36 +55,8 @@ A series of tests to test that the pencil can write to a string
 """
 @pytest.mark.parametrize(writing_test_labels, writing_test_data)
 def test_write_on_strings(pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
-	paper = [page_before_being_writen_on]
-	pencil.write(string_to_write, paper, string_starting_point)
-	assert expected_writing_on_page == paper[0]
-
-"""
-A series of tests to test that the pencil can write to a file
-"""
-@pytest.mark.parametrize(writing_test_labels, writing_test_data)
-def test_write_on_text_files(pencil, tmpdir, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
-	pencil.how_to_write = pencil.write_to_text_file
-	paper = tmpdir.mkdir("sub").join("paper.txt")
-	paper.write(page_before_being_writen_on)
-	pencil.write(string_to_write, paper, string_starting_point)
-	assert paper.read() == expected_writing_on_page
-
-"""
-A series of tests to test that the pencil can write on any object
-given to it so long as the pencil is told where to and how to write
-to the object
-"""
-@pytest.mark.parametrize(writing_test_labels, writing_test_data)
-def test_write_on_paper_object(pencil, expected_writing_on_page, page_before_being_writen_on, string_to_write, string_starting_point):
-	class Paper():
-		def __init__(self, text):
-			self.text = text
-
-	paper = Paper(page_before_being_writen_on)
-	pencil.how_to_write = pencil.write_to_object_variable
-	pencil.write(string_to_write, paper, string_starting_point, "text")
-	assert paper.text == expected_writing_on_page
+	paper = pencil.write(string_to_write, page_before_being_writen_on, string_starting_point)
+	assert expected_writing_on_page == paper
 
 """
 A series of tests to test the pencil durability and what happens when that durability runs out
@@ -92,9 +64,8 @@ A series of tests to test the pencil durability and what happens when that durab
 @pytest.mark.parametrize(point_degradation_test_labels, point_degradation_test_data)
 def test_point_degradation(expected_writing_on_page, expected_point_value, starting_point_value, page_before_being_writen_on, string_to_write, string_starting_point):
 	pencil = Pencil(starting_point_value, 1)
-	paper = [page_before_being_writen_on]
-	pencil.write(string_to_write, paper, string_starting_point)
-	assert expected_writing_on_page == paper[0]
+	paper = pencil.write(string_to_write, page_before_being_writen_on, string_starting_point)
+	assert expected_writing_on_page == paper
 	assert expected_point_value == pencil.point_durability
 
 """
@@ -103,14 +74,12 @@ A series of tests to test the sharpen functionality of the pencil
 @pytest.mark.parametrize(sharpen_test_labels, sharpen_test_data)
 def test_sharpen(expected_writing_on_page, expected_point_value, starting_point_value, expected_length, starting_length, page_before_being_writen_on, string_to_write, string_starting_point):
 	pencil = Pencil(starting_point_value, starting_length)
-	paper = [page_before_being_writen_on]
-	pencil.write(string_to_write, paper, string_starting_point)
+	pencil.write(string_to_write, page_before_being_writen_on, string_starting_point)
 	pencil.sharpen()
 	assert pencil.point_durability == expected_point_value
 	assert pencil.length == expected_length
 
 @pytest.mark.parametrize(erase_test_labels, erase_test_data)
 def test_erase(pencil, expected_writing_on_page, page_before_being_erased, string_to_erase):
-	paper = [page_before_being_erased]
-	pencil.erase(paper, string_to_erase)
-	assert paper[0] == expected_writing_on_page
+	paper = pencil.erase(page_before_being_erased, string_to_erase)
+	assert expected_writing_on_page == paper
